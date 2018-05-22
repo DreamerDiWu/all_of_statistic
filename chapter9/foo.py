4cddfa5947dfd3add0fd51abb1fc932a930071f8
woody <32692477+DreamerDiWu@users.noreply.github.com>
2018-05-22 21:26:31 +0800

    Delete foo.py

--- chapter9/foo.py
+++
@@ -1,9 +0,0 @@
-from scipy import stats
-import numpy as np 
-import seaborn as sns
-import matplotlib.pyplot as plt 
-if __name__ == '__main__':
-	a = stats.norm.rvs(size=100)
-	sns.distplot(a, label='foo')
-	plt.legend()
-	plt.show()
